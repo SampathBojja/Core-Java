76e65ba19074e3380d6757362b661032d894e8fe
sampath <sampath@DESKTOP-BMFAGF6>
2022-05-06 16:57:07 +0530

    first commit

---
+++ basic/src/basic/helloWorldProg.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class helloWorldProg {
+	public static void main(String[] args) {
+		System.out.println(2+3); 
+	}
+
+}
---
+++ basic/src/basic/prog1.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class prog1 {
+
+	public static void main(String[] args) {
+		int a = 210;
+		int b = 16;
+		System.out.println(a+b);
+
+	}
+
+}
